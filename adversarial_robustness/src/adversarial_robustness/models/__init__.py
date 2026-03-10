2fb3031457e94ed3b32595a710e5a9893f948349
Zhanrock <krittaphastaishan@gmail.com>
2026-03-10 14:49:25 +0800

    Delete adversarial_robustness directory

--- adversarial_robustness/src/adversarial_robustness/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Model wrappers and abstract base class."""
-
-from adversarial_robustness.models.base_model import BaseModel
-from adversarial_robustness.models.dummy_model import DummyClassifier, PytorchModelWrapper
-
-__all__ = ["BaseModel", "DummyClassifier", "PytorchModelWrapper"]
